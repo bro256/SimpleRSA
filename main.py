76f8cdc962d5a51b4071370f69b502ccb76b3144
Bronius <bronius256@gmail.com>
2024-10-10 22:34:56 +0300

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import Crypto
+
+bits = 128
+message = "Hello"
+
+msg = str(sys.argv[1])
+bits = int(sys.argv[2])
+
+p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+q = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+
+n = p * q
+PHI = (p - 1) * (q -1)
+e = 65537
+
+
+
