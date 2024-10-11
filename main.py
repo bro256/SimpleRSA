ef4283ad5da6f8e43bd70aa5b9d2129ac999e230
Bronius <bronius256@gmail.com>
2024-10-11 13:32:57 +0300

    refactor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-import Crypto
 import libnum
-from Crypto.Util.number import *
+from Crypto.Random import get_random_bytes
+from Crypto.Util.number import getPrime, bytes_to_long, long_to_bytes
 
 bits = 128
 msg = "Hello"
@@ -11,8 +11,8 @@ if (len(sys.argv)>1):
 if (len(sys.argv)>2):
         bits=int(sys.argv[2])
 
-p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
-q = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+p = getPrime(bits, randfunc=get_random_bytes)
+q = getPrime(bits, randfunc=get_random_bytes)
 
 n = p * q
 PHI = (p - 1) * (q -1)
@@ -25,4 +25,13 @@ c = pow(m, e, n)
 res = pow(c, d, n)
 
 
-print ("Message=%s\np=%s\nq=%s\n\nd=%d\ne=%d\nN=%s\n\nPrivate key (d,n)\nPublic key (e,n)\n\ncipher=%s\ndecipher=%s" % (msg,p,q,d,e,n,c,(long_to_bytes(res))))
+print(f"Message: {msg}")
+print(f"p: {p}")
+print(f"q: {q}")
+print(f"d: {d}")
+print(f"e: {e}")
+print(f"n: {n}")
+print(f"\nPrivate key: (d={d}, n={n})")
+print(f"Public key: (e={e}, n={n})")
+print(f"\nCipher: {c}")
+print(f"Deciphered message: {long_to_bytes(res).decode('utf-8')}")
