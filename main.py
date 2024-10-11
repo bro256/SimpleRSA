fddabc192c6f501536c0c7fdad5c1a7531bbfec3
Bronius <bronius256@gmail.com>
2024-10-11 11:46:14 +0300

    encryption and cecryption

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys
 import Crypto
 import libnum
+from Crypto.Util.number import *
 
 bits = 128
-message = "Hello"
+msg = "Hello"
 
-msg = str(sys.argv[1])
-bits = int(sys.argv[2])
+if (len(sys.argv)>1):
+        msg=str(sys.argv[1])
+if (len(sys.argv)>2):
+        bits=int(sys.argv[2])
 
 p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
 q = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
@@ -16,4 +19,10 @@ PHI = (p - 1) * (q -1)
 e = 65537
 d = libnum.invmod(e, PHI)
 
+m = bytes_to_long(msg.encode('utf-8'))
 
+c = pow(m, e, n)
+res = pow(c, d, n)
+
+
+print ("Message=%s\np=%s\nq=%s\n\nd=%d\ne=%d\nN=%s\n\nPrivate key (d,n)\nPublic key (e,n)\n\ncipher=%s\ndecipher=%s" % (msg,p,q,d,e,n,c,(long_to_bytes(res))))
